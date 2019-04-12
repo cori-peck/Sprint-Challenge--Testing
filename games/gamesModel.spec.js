c384552fbcf14407fd836019b358bf84b7943d2b
cori-peck <cori.peck@ymail.com>
2019-04-12 12:39:33 -0500

    insert test written, failing correctly

---
+++ games/gamesModel.spec.js
@@ -0,0 +1,19 @@
+const db = require('../data/dbConfig.js');
+const Games = require('./gamesModel.js');
+
+
+describe('games model', () => {
+    describe('insert()', () => {
+        afterEach(async () => {
+            await db('games').truncate();
+        })
+
+        it('should add new games into database', async () => {
+            await Games.insert({ title: 'Galaga', genre: "Shoot 'em up", releaseYear: 1981 })
+            await Games.insert({ title: 'Pong', genre: 'Sports game', releaseYear: 1972 })
+
+            const games = await db('games')
+            expect(games).toHaveLength(2);
+        })
+    })
+})
